8e541228f82bfd1f01494267e6cfc399fa61e754
cjwarrior7 <cj23021996@gmail.com>
2021-01-11 02:08:18 +0530

    Completed Challenge Add  code

--- Lect6_Async_Await/activity/challengeAdd.js
+++ Lect6_Async_Await/activity/challengeAdd.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const challenges = require('./challenge');
 var email = 'fakowa6828@vss6.com';
 var password = '123456789';
 let tab;
@@ -26,15 +27,19 @@ let tab;
    //await page.click('.active .backbone');
    let myChallengesLi = bothLis[1];
    await Promise.all([ page.waitForNavigation({waitUntil:"networkidle2"}) , myChallengesLi.click()]); // navigation
-   let addChallengeButton = await page.$('.btn.btn-green.backbone.pull-right');
-   console.log('cb'+addChallengeButton);
-
-   let addChallengeLink = await page.evaluate(function(element){ 
+    let addChallengeButton = await page.waitForSelector('.btn.btn-green.backbone.pull-right'); 
+    console.log('cb'+addChallengeButton);
+    let addChallengeLink = await page.evaluate(function(element){ 
        return element.getAttribute("href");
 
    }, addChallengeButton);
    completedLink = `https://www.hackerrank.com${addChallengeLink}`;
    console.log(''+completedLink);
+   for (let i = 0; i < challenges.length; i++) {
+       let newTab = await browser.newPage();
+       addchallenge(completedLink,newTab,challenges[i]);
+       
+   }
 
 
 }
@@ -45,3 +50,40 @@ console.log(error);
 
 
 })()
+async function addchallenge(link,newTab,challenge){
+    // "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
+    // "Description": "Question 1",
+    // "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
+    // "Input Format": "Integer",
+    // "Constraints": "n <= 10 ^ 9",
+    // "Output Format": "String",
+    // "Tags": "Basics"
+   
+    let challengeName = challenge["Challenge Name"];
+    let description = challenge["Description"];
+    let problemstatement = challenge["Problem Statement"];
+    let inputformat = challenge["Input Format"];
+    let constraints = challenge["Constraints"];
+    let outputformat = challenge["Output Format"];
+    let tags = challenge["Tags"];
+
+     await newTab.goto(link);
+     await newTab.waitForSelector('#name');
+     await newTab.type('#name',challengeName );
+     await newTab.type('#preview',description);
+     await newTab.waitForSelector('#problem_statement-container .CodeMirror.cm-s-default.CodeMirror-wrap textarea');
+     await newTab.type('#problem_statement-container .CodeMirror.cm-s-default.CodeMirror-wrap textarea',problemstatement);
+     await newTab.type('#input_format-container .CodeMirror.cm-s-default.CodeMirror-wrap textarea',inputformat);
+     await newTab.type(' #constraints-container .CodeMirror.cm-s-default.CodeMirror-wrap textarea',constraints);
+     await newTab.type('#output_format-container  .CodeMirror.cm-s-default.CodeMirror-wrap textarea',outputformat);
+     await newTab.type('#tags_tag ',tags);
+     await newTab.keyboard.press('Enter');
+     await newTab.click('.save-challenge.btn.btn-green');
+     await newTab.close();
+     console.log("All challenges submitted");
+
+
+     //newTab.type(,problemstatement)
+
+
+}
