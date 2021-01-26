f02f6e4cd1ddc00b5f326b471b7631d84cbb6594
cjwarrior7 <cj23021996@gmail.com>
2021-01-27 02:17:57 +0530

    Working on chat app

---
+++ Lect9_Real_Time_chat_app/activity/script.js
@@ -0,0 +1,25 @@
+let send = document.querySelector("#send");
+let chatMessage = document.querySelector("#send-chat");
+let chatlist = document.querySelector(".chat-list");
+chatMessage.addEventListener("keyup",function(e){
+console.log('Hi'+e.keyCode );
+if(e.keyCode == 13){
+        send.click();
+
+   }
+
+})
+
+send.addEventListener("click",function(){
+
+    if(chatMessage.value){
+        let chatdiv = document.createElement('div');
+        chatdiv.classList.add('chat');
+        chatdiv.classList.add('right');
+        chatdiv.innerHTML=chatMessage.value;
+        chatlist.append(chatdiv);
+        chatMessage.value = '';
+
+   }
+
+})
